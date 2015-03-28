9d2deda3124d19fd77a90eb97b2888e9b33ccfea
Markus Mengucci <markus.mengucci@interone.de>
2015-03-28 14:16:28 +0100

    added userId to addPet

--- server/methods/methods.js
+++ server/methods/methods.js
@@ -1,5 +1,12 @@
 Meteor.methods({
     addPet: function(newPet) {
+
+        if (! Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
+
+        newPet.owner = Meteor.userId();
+
         Pets.insert(newPet);
     }
 })
